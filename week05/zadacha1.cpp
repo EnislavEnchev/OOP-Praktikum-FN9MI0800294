df754b5867ce3188d089a54a5b26a0e62538ae63
EnislavEnchev <37375904+EnislavEnchev@users.noreply.github.com>
2023-03-21 10:56:49 +0200

    Add files via upload

---
+++ week05/zadacha1.cpp
@@ -0,0 +1,13 @@
+#include<fstream>
+#include<iostream>
+int main() {
+
+	std::ifstream if_stream;
+	if_stream.open("Doc.txt");
+
+	if_stream.seekg(0, std::ios::end);
+	long loc = if_stream.tellg();
+	std::cout << loc;
+
+	if_stream.close();
+}
