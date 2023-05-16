9df35aea132165e9dfac59ba2fc1e07dc63ecb25
EnislavEnchev <37375904+EnislavEnchev@users.noreply.github.com>
2023-05-16 12:05:04 +0300

    Add files via upload

--- week11/Car.cpp
+++ week11/Car.cpp
@@ -47,19 +47,7 @@ void Car::addPart(CarPart* part) {
 	parts[count] = part;
 	count++;
 }
-void Car::addPart(const CarPart& part) {
-	if (count == cap) {
-		cap *= 2;
-		CarPart** newParts = new CarPart * [cap];
-		for (size_t i = 0; i < count; i++) {
-			newParts[i] = parts[i];
-		}
-		delete[] parts;
-		parts = newParts;
-	}
-	parts[count] = part;
-	count++;
-}
+
 
 void Car::displayCarParts() {
 	for (size_t i = 0; i < count; i++) {
